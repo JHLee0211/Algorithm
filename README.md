4708a82b7779f82597c7ef46842a1cc0968357d8
Kwon <klw95@naver.com>
2020-10-19 16:19:43 +0900

    add 완주하지 못한 선수

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 > Java로 문제 풀이
 
-* 프로그래머스
+## 프로그래머스
 
-    * 완주하지 못한 선수
+* 해시 문제
 
-        https://programmers.co.kr/learn/courses/30/lessons/42576?language=java
+    * [완주하지 못한 선수](https://programmers.co.kr/learn/courses/30/lessons/42576?language=java)
 
-        
+    [풀이](https://github.com/JHLee0211/Algorithm/tree/khk/hash)
+
+    
+
+    
+
---
+++ hash/완주하지못한선수.java
@@ -0,0 +1,27 @@
+package javaAlgo.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for(String p: participant){
+                hm.put(p, hm.getOrDefault(p, 0)+1);
+        }
+        
+        for(String c: completion){
+            hm.put(c, hm.get(c)-1);
+        }
+        
+        for(Map.Entry<String, Integer> el: hm.entrySet()){
+            if(el.getValue() > 0){
+                answer = el.getKey();
+                break;
+            }
+        }
+        
+        return answer;
+    }
+}
